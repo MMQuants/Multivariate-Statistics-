83f42c9b560e31494cb4f53fc0e36a6689adb609
mmquants <mar4o86@mail.bg>
2022-11-22 12:35:01 +0200

    quick conf intervals

--- confidence intervals.R
+++ confidence intervals.R
@@ -8,3 +8,8 @@ SD <- sqrt(phat*(1-phat)/n)
 z <- qnorm(p=0.975, 0, 1)
 
 phat+c(-1,1)*z*SD
+
+# renaming confidence intervals values
+c(lower = phat-z*SD, upper = phat+z*SD )
+
+prop.test(x,n,conf.level = 0.975)
