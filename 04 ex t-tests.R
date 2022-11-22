1ee5896d6750a711bf6172eb967600038ba70a57
mmquants <mar4o86@mail.bg>
2022-11-22 10:03:35 +0200

    analysis of variance

--- 04 ex t-tests.R
+++ 04 ex t-tests.R
@@ -22,3 +22,14 @@ res$p.value
 res
 
 res$estimate
+
+data = data.frame(method = rep(c('a','b','c'), each=10),score=c(85,86,88,75,78,94,98,79,71,80,91,92,93,85,87,84,82,88,95,96,79,78,88,94,92,85,83,85,82,81))
+data
+
+# analysis of variance
+model1 = aov(score ~ method, data=data)
+
+# descriptive statistics
+model1
+
+summary(model1)
