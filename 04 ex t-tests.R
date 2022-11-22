3e90247232a5c7c9fb34fecaa6415471c5bf4ddf
mmquants <mar4o86@mail.bg>
2022-11-22 09:42:03 +0200

    t test with 2 samples

--- 04 ex t-tests.R
+++ 04 ex t-tests.R
@@ -3,3 +3,10 @@ mean(sample)
 var(sample)
 sqrt(var(sample))
 sd(sample)
+t.test(sample)
+t.test(sample, mu=5)
+
+# second sample a bigger one
+sample2 = rnorm(20,10,2)
+sample2
+t.test(sample, sample2)
