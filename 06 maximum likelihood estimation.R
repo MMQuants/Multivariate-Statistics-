8f48d47a9589c5fda4c682d84085f5465fdfa830
mmquants <mar4o86@mail.bg>
2022-11-29 09:50:05 +0200

    log likelihood function

--- 06 maximum likelihood estimation.R
+++ 06 maximum likelihood estimation.R
@@ -1,7 +1,9 @@
 library(UsingR)
 
-lik = function(z,param)
-prod(dnorm(z, mean = param[1], sd=param[2]))
+lik = function(z,param){
+  prod(dnorm(z, mean = param[1], sd=param[2])) 
+}
+
 
 lik(0,c(0,1))
 
@@ -12,3 +14,16 @@ z =rnorm(10)
 z
 
 lik(z,c(0,1))
+
+install.packages("maxLik")
+library(maxLik)
+
+x= rnorm(100, mean=1, sd=2)
+
+# log likelihood function
+logLikFun = function(param) {
+  mu = param[1]
+  sigma = param[2]
+  sum(dnorm(x, mean=mu, sd=sigme, log=TRUE))
+}
+
