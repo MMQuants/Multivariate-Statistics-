913fcea280cdaaaf0e762c0cf01b1fa9b012c432
mmquants <mar4o86@mail.bg>
2022-11-29 12:46:04 +0200

    prop.test

--- 07 hipotesis.R
+++ 07 hipotesis.R
@@ -1,6 +1,7 @@
 library(UsingR)
 
 n = 150000
+x = 22695
 phat = 22695/ n
 p0 = 0.15
 SD = sqrt(p0*(1-p0)/n)
@@ -10,3 +11,5 @@ z= (phat-p0)/ SD
 pnorm(z, lower.tail = FALSE)
 
 pnorm(phat, mean=p0, sd = SD, lower.tail = FALSE)
+
+prop.test(x,n,p=p0, alternative = 'greater')
