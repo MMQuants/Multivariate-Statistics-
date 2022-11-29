dad8ceeb9da24c902769f6e644b1949193117113
mmquants <mar4o86@mail.bg>
2022-11-29 09:17:41 +0200

    bartlett comparing 3 samples's variances

--- 05 var test.R
+++ 05 var test.R
@@ -1,4 +1,10 @@
 install.packages("UsingR")
 library(UsingR)
 head(ToothGrowth)
+
+# bartlett
+# compare more than 2 samples's variance
+bartlett.test(len~supp, dat=ToothGrowth)
+
+# compare the dose samples's variance (3 types)
 bartlett.test(len~supp, dat=ToothGrowth)
