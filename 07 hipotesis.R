519987e04168fdc28f0668f6d6461387094d403f
mmquants <mar4o86@mail.bg>
2022-11-29 12:43:53 +0200

    task 01

--- 07 hipotesis.R
+++ 07 hipotesis.R
@@ -1,2 +1,12 @@
 library(UsingR)
 
+n = 150000
+phat = 22695/ n
+p0 = 0.15
+SD = sqrt(p0*(1-p0)/n)
+
+z= (phat-p0)/ SD
+
+pnorm(z, lower.tail = FALSE)
+
+pnorm(phat, mean=p0, sd = SD, lower.tail = FALSE)
