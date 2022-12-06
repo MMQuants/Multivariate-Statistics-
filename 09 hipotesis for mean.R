d2c31df272265a5fdb077101af30ec77e6c03eac
mmquants <mar4o86@mail.bg>
2022-12-06 13:05:52 +0200

    Wilcox test for hypotesis

--- 09 hipotesis for mean.R
+++ 09 hipotesis for mean.R
@@ -1,6 +1,6 @@
 library(UsingR)
 
-# example: check if median is equal to 5
+# example 01: check if median is equal to 5
 calls = c(2,1,3,3,3,3,1,3,16,2,2,12,20,3,1)
 
 mean(calls)
@@ -11,3 +11,17 @@ n = length(calls)
 
 # reject the null hypothesis
 1-pbinom(n-obs-1, n, 1/2)
+
+# example 02 check if median is more than 0.005
+head(salmon.rate)
+plot(density(salmon.rate))
+
+plot(density(log(salmon.rate)))
+
+#check for normality
+qqnorm(log(salmon.rate))
+
+# Wilcox test check if median is more than 0.005
+# we cannot reject the  null hypothesis
+wilcox.test(log(salmon.rate), mu=log(0.005), n= length(salmon.rate), alternative = "greater")
+
