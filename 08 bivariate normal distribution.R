653c56e99cdc5605882109901cdd658f02a77f08
mmquants <mar4o86@mail.bg>
2022-12-06 09:46:17 +0200

    swiss data

--- 08 bivariate normal distribution.R
+++ 08 bivariate normal distribution.R
@@ -5,4 +5,17 @@ library(mvtnorm)
 
 sigma = matrix(c(4,2,2,3), nrow=2)
 x = rmvnorm(n=500, mean=c(1,2), sigma = sigma)
+head(x)
 plot(x)
+
+colMeans(x)
+var(x)
+
+data(swiss)
+help(swiss)
+head(swiss)
+
+colMeans(swiss[,2:3])
+colMeans(swiss)
+
+sapply(swiss[,2:3], sd)
