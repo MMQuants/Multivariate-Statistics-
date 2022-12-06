410d9f5f179770a87411311ccdcf5b870cc60d57
mmquants <mar4o86@mail.bg>
2022-12-06 13:39:55 +0200

    example 04 proportion hypothesis testing

--- 09 hipotesis for mean.R
+++ 09 hipotesis for mean.R
@@ -38,3 +38,17 @@ n = length(exec.pay)
 
 probability = 1 - pbinom(max(pay, n-pay)-1, n, 1/2)
 probability                     
+
+# example 04 proportion hypothesis testing
+phat = c(0.15, 0.1513)
+n = c(160000, 150000)
+
+# cannot reject the null hypothesis
+prop.test(n*phat, n, alternative = 'less')
+
+# with normal approximation
+p = sum(n*phat)/sum(n)
+
+z = (phat[1]-phat[2])/sqrt(p*(1-p)*sum(1/n))
+z
+pnorm(z)
