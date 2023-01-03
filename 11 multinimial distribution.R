ef1dec3ad2f22af7d06378107063c8ededa85720
mmquants <mar4o86@mail.bg>
2023-01-03 12:59:33 +0200

    chi squared test

--- 11 multinimial distribution.R
+++ 11 multinimial distribution.R
@@ -1,3 +1,4 @@
+library(UsingR)
 # task 01
 colors = c('blue', 'brown', 'green', 'orange', 'red', 'yellow', 'purple')
 
@@ -8,4 +9,23 @@ probs = probs / sum(probs)
 bag = sample(colors, 30, replace = TRUE, prob = probs)
 
 table(bag)
+
+# task 02
+
+y = c(35,40,25)
+
+p = c(35,35,30)
+
+p = p / sum(p)
+
+n = sum(y)
+
+# manual calculations
+chi2 = sum((y-n*p)^2 / n*p)
+
+pchisq(chi2, df=2, lower.tail = FALSE)
+
+
+# auto calculations
+chisq.test(y, p)
 
