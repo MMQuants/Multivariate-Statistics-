3477e00bc470caeb80e4c30ba65c12e03b031805
mmquants <mar4o86@mail.bg>
2023-01-03 09:33:06 +0200

    grouping by

--- 10 MANOVA.R
+++ 10 MANOVA.R
@@ -3,3 +3,7 @@ library(tidyverse)
 
 df=read_csv("https://reneshbedre.github.io/assets/posts/ancova/manova_data.csv")
 df
+
+df %>% 
+  group_by(plant_var) %>%  
+  summarise(n = n(), mean = mean(height), sd = sd(height))
