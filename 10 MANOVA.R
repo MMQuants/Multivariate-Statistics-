c78a160a5b993edcc6a3027c77a5d257a087048f
mmquants <mar4o86@mail.bg>
2023-01-03 09:37:30 +0200

    plotting

--- 10 MANOVA.R
+++ 10 MANOVA.R
@@ -7,3 +7,13 @@ df
 df %>% 
   group_by(plant_var) %>%  
   summarise(n = n(), mean = mean(height), sd = sd(height))
+
+df %>% 
+  group_by(plant_var) %>%  
+  summarise(n = n(), mean = mean(canopy_vol), sd = sd(canopy_vol))
+
+install.packages('gridExtra')
+library(gridExtra)
+p1 <- ggplot(df, aes(x = plant_var, y = height, fill = plant_var)) + geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2) + theme(legend.position="top")
+p2 <- ggplot(df, aes(x = plant_var, y = canopy_vol, fill = plant_var)) + geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2) + theme(legend.position="top")
+grid.arrange(p1, p2, ncol=2)
