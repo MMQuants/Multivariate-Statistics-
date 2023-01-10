137afd2439b9d9a9ac64c915ab12866c11537d44
mmquants <mar4o86@mail.bg>
2023-01-10 13:35:42 +0200

    task 01

--- 12 distribution tests.R
+++ 12 distribution tests.R
@@ -30,5 +30,20 @@ qqplot(sat.m, sat.v, main = "Math and Verbal SAT score")
 
 # shapiro test for normality 
 
-shapiro.test(sat.m)   # so it is not normaly distributed
-shapiro.test(sat.v)   # so it is not normaly distributed
+shapiro.test(sat.m)   # so it is not normally distributed
+shapiro.test(sat.v)   # so it is not normally distributed
+
+# task 01 check the height of babies for normal distribution
+
+head(babies)
+
+height = babies$ht[babies$ht < 99]
+shapiro.test(height)     # so it is not normally distributed
+
+weight = with(babies, wt[wt<999])
+weight
+shapiro.test(weight)     # so it is not normally distributed
+
+
+
+
