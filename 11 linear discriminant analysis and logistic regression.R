c189de70ca377cd3ca10682945a0ee9251d2cb10
mmquants <mar4o86@mail.bg>
2023-01-10 09:32:58 +0200

    data prep and plotting

--- 11 linear discriminant analysis and logistic regression.R
+++ 11 linear discriminant analysis and logistic regression.R
@@ -2,3 +2,15 @@ wines=read.table("C:/Users/grade/Desktop/wine.data",
                  sep=',')
 
 wines
+
+colors <- c("green", "red" ,"blue")[wines[ , 1]]
+colors
+
+newwine <- cbind( jitter(as.integer(wines[ , 1])), 	wines[ , -1])
+newwine
+
+names(newwine)[1] <- names(wines)[1] 
+names(newwine)[1] <- names(wines)[1] 
+head(newwine)
+
+pairs(newwine, pch = 16, cex = .3, gap = 0, col = colors, xaxt = "n", yaxt = "n")
