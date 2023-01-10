7f17af2c1222a361809eb2c39af0d47a91d0e0f6
mmquants <mar4o86@mail.bg>
2023-01-10 13:56:37 +0200

    task 01

--- 13 regression.R
+++ 13 regression.R
@@ -1,3 +1,5 @@
+library(UsingR)
+
 res.mhr = lm(maxrate ~ age, data = heartrate)
 res.mhr
 
@@ -24,4 +26,13 @@ predict(res.mhr, data.frame(age=c(20, 40)))
 # ANOVA
 anova(res.mhr)
 
+# task 01
+head(Cars93)
 
+model = lm(MPG.highway ~ Horsepower, data = Cars93 )
+model
+predict(model, data.frame(Horsepower = 225))
+
+model02 = lm(MPG.highway ~ Horsepower + Weight  , data = Cars93 )
+model02
+predict(model02, data.frame(Horsepower = 225, Weight = 5405))
