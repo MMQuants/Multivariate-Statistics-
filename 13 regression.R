d8f1ae2e179df885e3931ec4e87749a3ab29ce38
mmquants <mar4o86@mail.bg>
2023-01-10 14:02:11 +0200

    task 02

--- 13 regression.R
+++ 13 regression.R
@@ -36,3 +36,15 @@ predict(model, data.frame(Horsepower = 225))
 model02 = lm(MPG.highway ~ Horsepower + Weight  , data = Cars93 )
 model02
 predict(model02, data.frame(Horsepower = 225, Weight = 5405))
+
+# task 02
+height_2yr = c(39,30,32,34,35,36,36,30)
+
+height_adult = c(71,63,63,67,68,68,70,64)
+
+model03 = lm(height_adult ~ height_2yr)
+model03
+
+predict(model03, data.frame(height_2yr = 33))
+
+summary(model03)
