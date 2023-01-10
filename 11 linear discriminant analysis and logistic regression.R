49f9a60a6d5adf1b1baba6cb2a4b92f763c32867
mmquants <mar4o86@mail.bg>
2023-01-10 09:42:33 +0200

    analys of means

--- 11 linear discriminant analysis and logistic regression.R
+++ 11 linear discriminant analysis and logistic regression.R
@@ -14,3 +14,24 @@ names(newwine)[1] <- names(wines)[1]
 head(newwine)
 
 pairs(newwine, pch = 16, cex = .3, gap = 0, col = colors, xaxt = "n", yaxt = "n")
+
+wines %>% group_by(V1) %>%  summarise(n = n(), mean = mean(V2), sd = sd(V2))
+
+x=wines %>% group_by(V1) %>%  summarise(n = n(), 
+                                        mean2 = mean(V2),
+                                        mean3=mean(V3), 
+                                        mean4 = mean(V4),  
+                                        mean5=mean(V5),
+                                        mean6=mean(V6),
+                                        mean7=mean(V7),
+                                        mean8=mean(V8),
+                                        mean9=mean(V9),
+                                        mean10=mean(V10),
+                                        mean11=mean(V11),
+                                        mean12=mean(V12),
+                                        mean13=mean(V13),
+                                        mean14=mean(V14))
+
+x
+
+
