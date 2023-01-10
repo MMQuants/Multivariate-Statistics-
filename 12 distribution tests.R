9c66ffbd471d3332b4c097177011293e5f0ec58d
mmquants <mar4o86@mail.bg>
2023-01-10 13:23:46 +0200

    testing if 2 samples have the same distribution

--- 12 distribution tests.R
+++ 12 distribution tests.R
@@ -11,3 +11,13 @@ ks.test(x, "pnorm", mean = 0, sd = 2)
 
 ks.test(x, "pnorm", mean = 5, sd = 1)
 
+# we have 2 samples does they have identical distributions
+head(SAT)
+
+sat.m = SAT$math
+sat.v = SAT$verbal
+
+ks.test(sat.m, sat.v) # so they do not have the same distribution
+
+head(stud.recs)
+ks.test(stud.recs$sat.m, stud.recs$sat.v)
